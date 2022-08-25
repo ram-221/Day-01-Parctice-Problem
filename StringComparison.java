78cd6100fef5cf95d32044f3b0bf674675b7c0d3
ram-221 <rameshmalli857.com>
2022-08-25 22:05:05 +0530

    Check two strings are equal or not

---
+++ StringComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabday1;
+
+public class StringComparison {
+
+	public static void main(String args[]) {
+		String s1= "String Comparison";
+		String s2 = "String ComParison";
+		System.out.println(s1.equals(s2));
+	}
+}
